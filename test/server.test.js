521398d0488d5fd6e53ee04b377a08118a5dfd61
Luc Saunders <lucsaunders@gmail.com>
2019-04-11 03:12:27 -0400

    Update tests for public routes.

--- test/server.test.js
+++ test/server.test.js
@@ -2,7 +2,8 @@ const server = require('../app/server');
 
 const chai = require('chai'),
   chaiHttp = require('chai-http'),
-  expect = chai.expect;
+  expect = chai.expect,
+  assert = chai.assert;
 
 chai.use(chaiHttp);
 
@@ -10,34 +11,6 @@ chai.use(chaiHttp);
 // setTimeout(function() {
 //   run();
 // }, 10000);
-
-/***************************************
- *   TEST Empty query: GET / or GET /api
- ***************************************/
-describe('Empty queries (/)', () => {
-  it.skip('should return a suggestion to redirect to brands or products', done => {
-    chai
-      .request(server)
-      .get('/')
-      .end((error, response) => {
-        expect(response).to.have.status(200);
-        expect(response.body).to.be.an('object');
-        done();
-      });
-  });
-});
-describe('Empty queries (/api)', () => {
-  it.skip('should return a suggestion to redirect to brands or products', done => {
-    chai
-      .request(server)
-      .get('/api')
-      .end((error, response) => {
-        expect(response).to.have.status(200);
-        expect(response.body).to.be.a('string');
-        done();
-      });
-  });
-});
 
 /*==================================
  *   TEST Brands: GET /api/brands
@@ -48,6 +21,7 @@ describe('/GET brands', () => {
       .request(server)
       .get('/api/brands')
       .end((error, response) => {
+        // expect(200)???
         expect(response).to.have.status(200);
         expect('Content-Type', 'application/json');
         expect(response.body).to.be.an('array');
@@ -64,11 +38,25 @@ describe('/GET products by brand', () => {
   it('should return all products of the specified brand', done => {
     chai
       .request(server)
-      .get('/api/brands/:id/products')
+      // test case:  "categoryId": "5" (Burberry) should return 2 products
+      .get('/api/brands/5/products')
       .end((error, response) => {
+        // expect(200)???
         expect(response).to.have.status(200);
         expect('Content-Type', 'application/json');
         expect(response.body).to.be.an('array');
+        expect(response.body).to.have.lengthOf(2);
+        done();
+      });
+  });
+  it('should return 404 BRAND ID OR PRODUCTS NOT FOUND if there are no matches', done => {
+    chai
+      .request(server)
+      .get('/api/brands/6/products')
+      .end((error, response) => {
+        assert.exists(response.body);
+        // expect(404) ???
+        expect(response).to.have.status(404);
         done();
       });
   });
@@ -78,15 +66,42 @@ describe('/GET products by brand', () => {
  *   TEST Products: GET /api/products
  *=====================================*/
 describe('/GET products', () => {
-  it('should return all products of all brands', done => {
+  it('should return all products of all brands if query is empty', done => {
     chai
       .request(server)
       .get('/api/products')
       .end((error, response) => {
+        // expect(200)???
         expect(response).to.have.status(200);
         expect('Content-Type', 'application/json');
         expect(response.body).to.be.an('array');
         expect(response.body).to.have.lengthOf(11);
+        done();
+      });
+  });
+  it('should return 404 BRANDS NOT FOUND if query exists but has no matches', done => {
+    chai
+      .request(server)
+      // test case: query "bartfarf"
+      .get('/api/products?query=bartfarf')
+      .end((error, response) => {
+        assert.exists(response.body);
+        // expect(400); ???
+        expect(response).to.have.status(404);
+        done();
+      });
+  });
+  it('should return all products specified by query', done => {
+    chai
+      .request(server)
+      // test case: "best"
+      .get('/api/products?query=best')
+      .end((error, response) => {
+        // expect(200) ???
+        expect(response).to.have.status(200);
+        expect('Content-Type', 'application/json');
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.lengthOf(4);
         done();
       });
   });
@@ -95,24 +110,36 @@ describe('/GET products', () => {
 /*====================================
  *   TEST User login: POST /api/login
  *====================================
-    REVISED VERSION
+    REVISED 
 -------------------------------------*/
 describe('/POST user login', () => {
   it('should succeed if username and password are both valid', done => {
     chai
       .request(server)
       .post('/api/login')
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .send({ username: 'username', password: 'password' })
+      // .set('Accept', 'application/json')
+      // .set('Content-Type', 'application/json')
+      .send({ username: 'greenlion235', password: 'waters' })
       .end((error, response) => {
-        // expect(200);
+        assert.isNull(error);
+        // expect(200); ???
         expect(response).to.have.status(200);
-        expect('Content-Type', /json/);
-        expect(function(response) {
-          expect(response.body).not.to.be.empty;
-          // expect(response.body).to.be.an('object');
-        });
+        expect('Content-Type', 'application/json');
+        // expect(function(response) {
+        // expect(response.body).not.to.be.empty;
+        // expect(response.body).to.be.an('object');
+      });
+    done();
+  });
+  it('should return 401 error if username or password is invalid', done => {
+    chai
+      .request(server)
+      .post('/api/login')
+      .send({ username: 'invalid', password: 'equallyInvalid' })
+      .end((error, response) => {
+        assert.isNull(error);
+        // expect(401); ???
+        expect(response).to.have.status(401);
         done();
       });
   });
